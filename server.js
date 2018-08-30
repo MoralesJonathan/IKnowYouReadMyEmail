67c8d428ec1e60295f614db0d2400a33c1b58130
Jonathan Morales <jonmorazav@Yahoo.com>
2018-08-30 10:40:46 -0400

    adding image and route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
   server = express()
   port = process.env.PORT || 8080
   environment = server.get('env')
+  path = require("path")
   logger = require('morgan')
   fs = require('fs')
   keys = require('./keys.json');
@@ -11,11 +12,9 @@ server
   .use(logger('dev'));
 
 server
-  .get('/', (req, res) => {
-    res.send('Nice!');
-  })
-  .post('/', (req, res) => {
-    res.send('Nice!');
+  .get('/image/:emailAccount/:recipient/:subject', (req, res) => {
+    console.log(`Email sent from account: ${req.params.emailAccount} was read by: ${req.params.recipient} for email subject: ${req.params.subject}`)
+    res.sendFile('track.jpg', { root: path.join(__dirname, '/public') });
   })
 
 server.listen(port, () => {
